29b5de8ba6fc1c2b55e7a69c1cfdf639751c3392
enkaypeter <enkaypeter@gmail.com>
2025-06-10 18:58:15 +0100

    remove unnecessary comment

--- src/hooks/usePersistentState.ts
+++ src/hooks/usePersistentState.ts
@@ -1,12 +1,5 @@
 import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 
-/**
- * A custom React hook that persists state to localStorage.
- *
- * @param key The key to use for storing the value in localStorage.
- * @param initialValue The initial value to use if no value is found in localStorage or if parsing fails.
- * @returns A stateful value, and a function to update it, similar to useState.
- */
 function usePersistentState<T>(
   key: string,
   initialValue: T
